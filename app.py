4de6c067ba0ede3e894b16832536ed3ff22d9951
Ken-Joaqui <bondoc.kenjoaqui@gmail.com>
2025-07-28 03:30:52 +0800

    add /login route to process form submission

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+VALID_USERNAME = "admin"
+VALID_PASSWORD = "1234"
+
 @app.route('/')
 def home ():
 	return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login ():
+	username = request.form['username']
+	password = request.form['password']
+
+	if username == VALID_USERNAME and password == VALID_PASSWORD:
+		return "<h2>Login Succesful!</h2>"
+	else:
+		return "<h2>Invalid username or password</h2>"
 
 if __name__ == '__main__':
 	app.run(debug=True)
