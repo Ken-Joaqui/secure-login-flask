d630a35b3e39956fbe6b92f16f8c9f05f61d4975
Ken-Joaqui <bondoc.kenjoaqui@gmail.com>
2025-07-28 21:02:42 +0800

    Added login attempt limit to block brute force attacks

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+login_attempts = {}
+MAX_ATTEMPTS = 3
+
 fake_users = {
 	"admin": generate_password_hash("1234"),
 	"ken": generate_password_hash("pogi"),
@@ -18,10 +21,18 @@ def login ():
 	username = request.form['username']
 	password = request.form['password']
 
+	if username not in login_attempts:
+		login_attempts[username] = 0
+	
+	if login_attempts[username] >= MAX_ATTEMPTS:
+		return "<h2>Too many failed attempts. Access Blocked.</h2>"
+
 	if username in fake_users and check_password_hash(fake_users[username], password):
+		login_attempts[username] = 0
 		return "<h2>Login Succesful!</h2>"
 	else:
-		return "<h2>Invalid username or password</h2>"
+		login_attempts[username] += 1
+		return f"<h2>Invalid login. Attempt {login_attempts[username]}/{MAX_ATTEMPTS}</h2>"
 
 if __name__ == '__main__':
 	app.run(debug=True)
