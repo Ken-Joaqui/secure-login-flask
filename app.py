b2cbc221e420405f175a96116c62e52117e81ead
Ken-Joaqui <bondoc.kenjoaqui@gmail.com>
2025-07-28 20:46:35 +0800

    Added fake user database with hashed passwords

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-VALID_USERNAME = "admin"
-HASHED_PASSWORD = generate_password_hash("1234")
+fake_users = {
+	"admin": generate_password_hash("1234"),
+	"ken": generate_password_hash("pogi"),
+	"noone": generate_password_hash("who")
+}
 
 @app.route('/')
 def home ():
@@ -15,7 +18,7 @@ def login ():
 	username = request.form['username']
 	password = request.form['password']
 
-	if username == VALID_USERNAME and check_password_hash(HASHED_PASSWORD, password):
+	if username in fake_users and check_password_hash(fake_users[username], password):
 		return "<h2>Login Succesful!</h2>"
 	else:
 		return "<h2>Invalid username or password</h2>"
